28498da1f2145a0de48038cde097cf869baa01f3
maxlotz <maxlotz@gmail.com>
2018-11-24 11:49:48 +0100

    transition function & reward function created & tested, should work

--- lab1.py
+++ lab1.py
@@ -9,9 +9,16 @@ class Game():
         self.time = time
         self.player = player
         self.enemy = enemy
-        self.pij = self.calc_pij(time)
-        print(np.sum(self.pij))
-        print(np.sum(np.where(self.pij>0)))
+        self.exit_pos = [4,4]
+        self.S_dim = self.time*5*6*5*6 # state dimension
+        self.init_v = np.zeros(self.S_dim) # initial state values
+        self.init_p = np.ones((self.S_dim,len(self.player.actions))) * \
+                      1.0/len(self.player.actions) # initial policy
+        self.r_not_escaped = -1.0
+        self.r_eaten = -1000.0
+        self.r_escaped = 0.0
+        self.pij = self.calc_pij()
+        self.rewards = self.calc_rewards()
 
     def tostate(self, t, pos_p, pos_e):
         return 900*t + 30*(pos_p[0]*6 + pos_p[1]) + pos_e[0]*6 + pos_e[1]
@@ -27,21 +34,51 @@ class Game():
         x_e = rem2 % 6
         return T, [y_p, x_p], [y_e, x_e]
     
-    def calc_pij(self, T):
-        dim = T*5*6*5*6
-        pij = np.zeros((dim,dim,len(self.player.actions)))
+    def calc_pij(self):
+        pij = np.zeros((self.S_dim,self.S_dim,len(self.player.actions)))
         ''' Iterates through time, player_pos, enemy_pos and new_enemy_pos, because we know that new_time = time + 1 and we know new_player_pos from player.transition function.'''
-        iters = [T, 5, 6, 5, 6, 5, 6]
+        iters = [self.time, 5, 6, 5, 6, 5, 6]
         ranges = [range(x) for x in iters]
         for t, y_p, x_p, y_e, x_e, yn_e, xn_e in product(*ranges):
             for idx, action in enumerate(self.player.actions):
-                if t < T-1:
+                S = self.tostate(t,[y_p,x_p],[y_e,x_e])
+                if t < self.time-1:
                     posn_p = self.player.transition([y_p,x_p], action)
                     prob = self.enemy.transition([y_e,x_e],[yn_e,xn_e])
-                    S = self.tostate(t,[y_p,x_p],[y_e,x_e])
                     Sn = self.tostate(t+1, posn_p, [yn_e,xn_e])
                     pij[S,Sn,idx] = prob
+                ''' terminal states are recursive: final time step, minotaur kills player, player escapes maze '''
+                if t == self.time-1 or \
+                   y_p == y_e and x_p == x_e or \
+                   [y_p, x_p] == self.exit_pos:
+                    pij[S,S,idx] = 1.0
         return pij
+
+    def test_pij(self):
+        pass
+
+    def calc_rewards(self):
+        rewards = np.ones(self.S_dim)*self.r_not_escaped
+        iters = [self.time, 5, 6, 5, 6]
+        ranges = [range(x) for x in iters]
+        for t, y_p, x_p, y_e, x_e in product(*ranges):
+            S = self.tostate(t,[y_p,x_p],[y_e,x_e])
+            if t == self.time-1 or \
+               y_p == y_e and x_p == x_e:
+                rewards[S] = self.r_eaten
+            if [y_p, x_p] == self.exit_pos:
+                rewards[S] = self.r_escaped
+        return rewards
+
+    def test_rewards(self):
+        iters = [self.time, 5, 6, 5, 6]
+        ranges = [range(x) for x in iters]
+        for t, y_p, x_p, y_e, x_e in product(*ranges):
+            S = self.tostate(t,[y_p,x_p],[y_e,x_e])
+            print('t = {}, player = [{},{}], minotaur = [{},{}], reward = {}'.format(t, y_p, x_p, y_e, x_e, self.rewards[S]))
+
+    def find_optimal(self):
+        pass
 
     def display_board(self):
         vis_board = np.empty((5,6), dtype='str')
@@ -99,6 +136,13 @@ class Player():
         else:
             pos_new = [y + x for y, x in zip(pos, self.actdict[action])]
         return pos_new
+
+    def test_transition(self):
+        for y in range(5):
+            for x in range(6):
+                for action in self.actions:
+                    yn, xn = Gary.transition([y, x], action)
+                    print('[{},{}] > [{},{}] via {}'.format(y,x,yn,xn,action))
 
 
 class Enemy():
@@ -196,23 +240,13 @@ class Enemy():
                 prob = 0.0
         return prob
 
+    def test_transition(self):
+        iters = [5, 6, 5, 6]
+        ranges = [range(x) for x in iters]
+        for y, x, yn, xn in product(*ranges):
+            prob = self.transition([y,x],[yn,xn])
+            print('p{{({},{})|({},{})}} = {}'.format(yn,xn,y,x,prob))
+
 Gary = Player()
 Minotaur = Enemy()
 TheGame = Game(Gary, Minotaur)
-
-'''
-# TEST TO SEE RESULT OF ACTIONS
-for y in range(5):
-    for x in range(6):
-        for action in Gary.actdict.keys():
-            yn, xn = Gary.transition([y, x], action)
-            print('({},{}) > ({},{}) via {}'.format(y,x,yn,xn,action))
-
-# TEST TO SEE STATE TRANSITIONS
-for y in range(5):
-    for x in range(6):
-        for yn in range(5):
-            for xn in range(6):
-                prob = Minotaur.transition([y,x],[yn,xn])
-                print('p{{({},{})|({},{})}} = {}'.format(yn,xn,y,x,prob))
-'''
