479f7bd87dd372c6fb7649feb504e61449094554
maxlotz <maxlotz@gmail.com>
2018-11-25 16:01:07 +0100

    made some base classes to make it easier for q2/3

--- lab1.py
+++ lab1.py
@@ -3,58 +3,43 @@ import time
 import numpy as np
 from itertools import product
 
-class Game():
+class GameBase():
     """docstring for ClassName"""
-    def __init__(self, player, enemy, time=2):
+    def __init__(self, player, enemy, time=None):
         self.time = time
         self.player = player
         self.enemy = enemy
-        self.exit_pos = [4,4]
-        self.S_dim = self.time*5*6*5*6 # state dimension
+
+    def init_2(self):
+        if self.time:
+            self.S_dim = self.time*((self.W*self.H)**2) # state dimension
+        else:
+            self.S_dim = (self.W*self.H)**2
         self.init_v = np.zeros(self.S_dim) # initial state values
         self.init_p = np.ones((self.S_dim,len(self.player.actions))) * \
                       1.0/len(self.player.actions) # initial policy
-        self.r_not_escaped = -1.0
-        self.r_eaten = -1000.0
-        self.r_escaped = 0.0
         self.pij = self.calc_pij()
         self.rewards = self.calc_rewards()
 
-    def tostate(self, t, pos_p, pos_e):
-        return 900*t + 30*(pos_p[0]*6 + pos_p[1]) + pos_e[0]*6 + pos_e[1]
+    def tostate(self, pos_p, pos_e, t=None):
+        if t:
+            return ((self.H*self.W)**2)*t + \
+                   self.H*self.W*(pos_p[0]*self.W + pos_p[1]) + \
+                   pos_e[0]*self.W + pos_e[1]
+        else:
+            return self.H*self.W*(pos_p[0]*self.W + pos_p[1]) + \
+                   pos_e[0]*self.W + pos_e[1]
 
     def fromstate(self, s):
-        T = s // 900
-        rem = s % 900
-        pos_p = rem // 30
-        y_p = pos_p // 6
-        x_p = pos_p % 6
-        rem2 = rem % 30
-        y_e = rem2 // 6
-        x_e = rem2 % 6
-        return T, [y_p, x_p], [y_e, x_e]
-    
-    def calc_pij(self):
-        pij = np.zeros((self.S_dim,self.S_dim,len(self.player.actions)))
-        ''' Iterates through time, player_pos, enemy_pos and new_enemy_pos, because we know that new_time = time + 1 and we know new_player_pos from player.transition function.'''
-        iters = [self.time, 5, 6, 5, 6, 5, 6]
-        ranges = [range(x) for x in iters]
-        for t, y_p, x_p, y_e, x_e, yn_e, xn_e in product(*ranges):
-            for idx, action in enumerate(self.player.actions):
-                S = self.tostate(t,[y_p,x_p],[y_e,x_e])
-                if self.is_terminal(t,[y_p,x_p],[y_e,x_e]):
-                    pij[S,S,idx] = 1.0
-                else:
-                    posn_p = self.player.transition([y_p,x_p], action)
-                    prob = self.enemy.transition([y_e,x_e],[yn_e,xn_e])
-                    Sn = self.tostate(t+1, posn_p, [yn_e,xn_e])
-                    pij[S,Sn,idx] = prob
-                ''' terminal states are recursive: final time step, minotaur kills player, player escapes maze '''
-                
-        return pij
-
-    def is_terminal(self, t, pos_p, pos_e):
-        return t == self.time-1 or pos_p == pos_e or pos_p == self.exit_pos
+        t = s // ((self.H*self.W)**2)
+        rem = s % ((self.H*self.W)**2)
+        pos_p = rem // (self.H*self.W)
+        y_p = pos_p // self.W
+        x_p = pos_p % self.W
+        rem2 = rem % (self.H*self.W)
+        y_e = rem2 // self.W
+        x_e = rem2 % self.W
+        return t, [y_p, x_p], [y_e, x_e]
 
     def test_pij(self):
         # some sanity checks
@@ -109,12 +94,54 @@ class Game():
         print('fraction of non zero elements = {:.5f}\n\n'
               .format(non_zero_frac))
 
-    def calc_rewards(self):
-        rewards = np.ones(self.S_dim)*self.r_not_escaped
-        iters = [self.time, 5, 6, 5, 6]
+    def test_rewards(self):
+        t = self.time if self.time else 1
+        iters = [t, self.W, self.H, self.W, self.H]
         ranges = [range(x) for x in iters]
         for t, y_p, x_p, y_e, x_e in product(*ranges):
-            S = self.tostate(t,[y_p,x_p],[y_e,x_e])
+            S = self.tostate([y_p,x_p],[y_e,x_e],t)
+            print('t = {}, player = [{},{}], minotaur = [{},{}], reward = {}'.format(t, y_p, x_p, y_e, x_e, self.rewards[S]))
+
+class Ex1Game(GameBase):
+    """docstring for ClassName"""
+    def __init__(self, *args, **kwargs):
+        super(Ex1Game, self).__init__(*args, **kwargs)
+        self.H, self.W = 5, 6
+        self.exit_pos = [4,4]
+        self.r_not_escaped = -1.0
+        self.r_eaten = -1000.0
+        self.r_escaped = 0.0
+        self.init_2()
+    
+    def calc_pij(self):
+        pij = np.zeros((self.S_dim,self.S_dim,len(self.player.actions)))
+        ''' Iterates through time, player_pos, enemy_pos and new_enemy_pos, because we know that new_time = time + 1 and we know new_player_pos from player.transition function.'''
+        iters = [self.time, self.H, self.W, self.H, self.W, self.H, self.W]
+        ranges = [range(x) for x in iters]
+        for t, y_p, x_p, y_e, x_e, yn_e, xn_e in product(*ranges):
+            for idx, action in enumerate(self.player.actions):
+                S = self.tostate([y_p,x_p],[y_e,x_e],t)
+                if self.is_terminal([y_p,x_p],[y_e,x_e],t):
+                    pij[S,S,idx] = 1.0
+                else:
+                    posn_p = self.player.transition([y_p,x_p], action, 
+                                                    self.H, self.W)
+                    prob = self.enemy.transition([y_e,x_e],[yn_e,xn_e])
+                    Sn = self.tostate(posn_p, [yn_e,xn_e], t+1)
+                    pij[S,Sn,idx] = prob
+                ''' terminal states are recursive: final time step, minotaur kills player, player escapes maze '''
+                
+        return pij
+
+    def is_terminal(self, pos_p, pos_e, t):
+        return t == self.time-1 or pos_p == pos_e or pos_p == self.exit_pos
+
+    def calc_rewards(self):
+        rewards = np.ones(self.S_dim)*self.r_not_escaped
+        iters = [self.time, self.H, self.W, self.H, self.W]
+        ranges = [range(x) for x in iters]
+        for t, y_p, x_p, y_e, x_e in product(*ranges):
+            S = self.tostate([y_p,x_p],[y_e,x_e],t)
             if t == self.time-1 or \
                y_p == y_e and x_p == x_e:
                 rewards[S] = self.r_eaten
@@ -122,18 +149,11 @@ class Game():
                 rewards[S] = self.r_escaped
         return rewards
 
-    def test_rewards(self):
-        iters = [self.time, 5, 6, 5, 6]
-        ranges = [range(x) for x in iters]
-        for t, y_p, x_p, y_e, x_e in product(*ranges):
-            S = self.tostate(t,[y_p,x_p],[y_e,x_e])
-            print('t = {}, player = [{},{}], minotaur = [{},{}], reward = {}'.format(t, y_p, x_p, y_e, x_e, self.rewards[S]))
-
     def find_optimal(self):
         pass
 
     def display_board(self):
-        vis_board = np.empty((5,6), dtype='str')
+        vis_board = np.empty((self.H,self.W), dtype='str')
         vis_board[:] = ' '
         if self.player.pos == self.enemy.pos:
             vis_board[tuple(self.player.pos)] = 'B' # Both in same position
@@ -151,10 +171,10 @@ class Game():
         time.sleep(0.3)
 
 
-class Player():
+class PlayerBase():
     """docstring for ClassName"""
     def __init__(self):
-        self.pos = [0,0]
+        self.pos = None
         self.actions = ['U','D','L','R','S']
         self.actdict = {'U': (-1,0),
                         'D': (1,0),
@@ -162,17 +182,44 @@ class Player():
                         'R': (0,1),
                         'S': (0,0)}
 
-    def transition(self, pos, action):
+    def test_transition(self, H, W):
+        for y in range(H):
+            for x in range(W):
+                for action in self.actions:
+                    yn, xn = self.transition([y, x], action, H, W)
+                    print('[{},{}] > [{},{}] via {}'.format(y,x,yn,xn,action))
+
+    def into_edge(self, pos, action, H, W):
+        # returns true if action walks player into edge of map
+        y, x = pos[0], pos[1]
+        if (y == 0 and action == 'U') or \
+           (y == (H - 1) and action == 'D') or \
+           (x == 0 and action == 'L') or \
+           (x == (W - 1) and action == 'R'):
+            return True
+        else:
+            return False
+
+    def transition(self, pos, action, H, W):
         # ACCEPTS: old position, action
         # RETURNS: new position
         # NOTE: assumes invalid action returns same position!
+        if self.into_edge(pos, action, H, W):
+            pos_new = [y,x]
+        else:
+            [y + x for y, x in zip(pos, self.actdict[action])]
+
+class Ex1Player(PlayerBase):
+    """docstring for Ex1Player"""
+    def __init__(self):
+        super(Ex1Player, self).__init__()
+        self.pos = [0,0]
+
+    def transition(self, pos, action, H, W):
         y, x = pos[0], pos[1]
         # edge proximity actions
-        if (y == 0 and action == 'U') or \
-           (y == 4 and action == 'D') or \
-           (x == 0 and action == 'L') or \
-           (x == 5 and action == 'R'):
-           pos_new = [y,x]
+        if self.into_edge(pos, action, H, W):
+            pos_new = [y,x]
         # wall proximity actions going left to right, top to bottom
         elif (x == 1 and y < 3 and action == 'R') or \
              (x == 2 and y < 3 and action == 'L') or \
@@ -184,25 +231,26 @@ class Player():
              (y == 4 and x > 0 and x < 5 and action == 'U') or \
              (y == 4 and x == 3 and action == 'R') or \
              (y == 4 and x == 4 and action == 'L'):
-             pos_new = [y,x]
+            pos_new = [y,x]
         else:
             pos_new = [y + x for y, x in zip(pos, self.actdict[action])]
         return pos_new
-
-    def test_transition(self):
-        for y in range(5):
-            for x in range(6):
-                for action in self.actions:
-                    yn, xn = Gary.transition([y, x], action)
-                    print('[{},{}] > [{},{}] via {}'.format(y,x,yn,xn,action))
+        
 
 
-class Enemy():
+class Ex1Enemy():
     """docstring  for ClassName"""
     def __init__(self, can_same=False):
-        self.pos = [4,4]
+        self.pos = [4,5]
         self.can_same = can_same # Can it remain in same place, see transition
 
+    def test_transition(self, H, W):
+        iters = [H, W, H, W]
+        ranges = [range(x) for x in iters]
+        for y, x, yn, xn in product(*ranges):
+            prob = self.transition([y,x],[yn,xn])
+            print('p{{({},{})|({},{})}} = {}'.format(yn,xn,y,x,prob))
+        
     def transition(self, oldpos, newpos):
         # ACCEPTS: old position, new position
         # RETURNS: probability of transition
@@ -292,14 +340,10 @@ class Enemy():
                 prob = 0.0
         return prob
 
-    def test_transition(self):
-        iters = [5, 6, 5, 6]
-        ranges = [range(x) for x in iters]
-        for y, x, yn, xn in product(*ranges):
-            prob = self.transition([y,x],[yn,xn])
-            print('p{{({},{})|({},{})}} = {}'.format(yn,xn,y,x,prob))
+time = 15
+Theseus, Minotaur = Ex1Player(), Ex1Enemy()
+MazeEscape = Ex1Game(Theseus, Minotaur, time)
+MazeEscape.test_pij()
 
-Gary = Player()
-Minotaur = Enemy()
-TheGame = Game(Gary, Minotaur)
-TheGame.test_pij()
+#TheGame = Game(Theseus, Minotaur)
+#TheGame.test_pij()
