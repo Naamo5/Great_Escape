fab488a56d533b8235bf9faf65442a432316a654
Francçois Le Rall <lerall.francois@gmail.com>
2018-11-28 19:15:03 +0100

    Ex1.2 finalize

--- lab1.py
+++ lab1.py
@@ -2,6 +2,7 @@ import random
 import time
 import numpy as np
 from itertools import product
+import matplotlib.pyplot as plt
 
 class Game():
     """docstring for ClassName"""
@@ -12,8 +13,8 @@ class Game():
         self.S_dim = 5*6*5*6+2 # state dimension all combinaison of position + eaten + escaped
         self.init_v = np.zeros(self.S_dim) # initial state values
         self.r_not_escaped = 0.0
-        self.r_eaten = -1.0
-        self.r_escaped = 2.0
+        self.r_eaten = 0.0
+        self.r_escaped = 1.0
         self.pij = self.calc_pij()
         self.rewards = self.calc_rewards()
 
@@ -46,10 +47,12 @@ class Game():
                 Sn = self.tostate(posn_p, [yn_e,xn_e])
                 pij[S,Sn,idx] = prob
                 ''' terminal states are recursive: final time step, minotaur kills player, player escapes maze '''
-                if y_p == y_e and x_p == x_e:
-                    pij[S,self.S_dim-2,idx] = 1.0
-                elif [y_p, x_p] == self.exit_pos:
+                if [y_p, x_p] == self.exit_pos:
+                    pij[S,Sn,idx] = 0
                     pij[S,self.S_dim-1,idx] = 1.0
+                if y_p == y_e and x_p == x_e:
+                    pij[S,Sn,idx] = 0
+                    pij[S,self.S_dim-2,idx] = 1.0
         """ When the player is already eaten or already escaped, whatever he does, he will stay in the same state"""
         for idx, action in enumerate(self.player.actions):
             #When he was eaten
@@ -63,7 +66,7 @@ class Game():
         for s in range(self.S_dim-2):
             [y_p,x_p],[y_e,x_e] = self.fromstate(s)
             if [y_p, x_p] == self.exit_pos:
-                rewards[s,:] = self.r_escaped
+                rewards[s,4] = self.r_escaped
             if y_p == y_e and x_p == x_e:
                 rewards[s] = [self.r_eaten]*self.player.A_dim
         return rewards
@@ -86,54 +89,98 @@ class Game():
         else:
             vis_board[tuple(self.player.pos)] = 'P' # Player
             vis_board[tuple(self.enemy.pos)] = 'M' # Minotaur
-        print(' ' + '_'*8 + ' ')
-        print('|{}{}|{}{} {}{}|'.format(*vis_board[0,:]))
-        print('|{}{}|{}{}|{}{}|'.format(*vis_board[1,:]))
-        print('|{}{}|{}{}|{}\u0305{}\u0305|'.format(*vis_board[2,:]))
-        print('|{}{} {}{} {}{}|'.format(*vis_board[3,:]))
-        print('|{}{}\u203e{}\u0305{}\u0305|\u0305{}\u0305{}\u0305|'.format(
+        print('╭' + '─'*5 +'┬' + '─'*11 + '╮') #╭─────────────────╮
+        print('│ {} {} │ {} {}   {} {} │'.format(*vis_board[0,:])) #│
+        print('│ {} {} │ {} {} │ {} {} │'.format(*vis_board[1,:]))
+        print('│     │     ├─────┤')
+        print('│ {} {} │ {} {} │ {} {} │'.format(*vis_board[2,:]))
+        print('│ {} {}   {} {}   {} {} │'.format(*vis_board[3,:]))
+        print('│   ────────┬───  │')
+        print('│ {} {}   {} {} │ {} {} │'.format(
             *vis_board[4,:]))
-        print(' ' + '\u203e'*8 + ' ')
-        time.sleep(0.2)
+        print('╰' + '─'*11 +'┴'+'─'*5+ '╯') #╰─────────────────╯
+        time.sleep(0.6)
 
     def BW_induction(self, time):
         """ Back ward induction algorithm for a finite horizon MDP problem """
         u2 = np.zeros((self.S_dim, time))
         """ Setting of the last colomn (for final time) of the value function  """
-        u2[:,time-1] = [np.max([self.rewards[s,a] for a in range(self.player.A_dim)]) for s in range(self.S_dim)]
+        u2[:,-1] = [np.max([self.rewards[s,a] for a in range(self.player.A_dim)]) for s in range(self.S_dim)]
         """ The initial policy is to Stay """
-        policy = 4*np.ones((self.S_dim, time))
+        policy = 5*np.ones((self.S_dim, time-1))
         """ Iterates through time, state and action """
-        for t in range(1,time):
-            """ Starting from T-1 to 0 """
-            print(time-1-t)
-            u1 = np.zeros(self.S_dim)
-            for s1 in range(self.S_dim):
-                u_temp = np.zeros(self.player.A_dim)
-                for a in range(self.player.A_dim):
-                    u_temp[a] = sum([self.pij[s1,s2,a] * (self.rewards[s1,a] + u2[s2,time-t]) for s2 in range(self.S_dim)])
-                u1[s1] = max(u_temp) #value function for s1 at time-1-t
-                policy[s1,time-1-t] = np.argmax(u_temp) #optimal policy for s1 at time-1-t
-            u2[:,time-1-t] = np.copy(u1)
+        for t in reversed(range(1,time)):
+            """ Starting from indice T-1 to 1 """
+            utemp = np.zeros((self.S_dim,self.player.A_dim))
+            for a in range(self.player.A_dim):
+                utemp[:,a] = self.rewards[:,a] + np.matmul(self.pij[:,:,a],u2[:,t])
+            u1 = [np.max(utemp[s,:]) for s in range(self.S_dim)] #value function for s1 at time t-1
+            policy[:,t-1] = [np.argmax(utemp[s,:]) for s in range(self.S_dim)] #optimal policy for s1 at time-1-t
+            u2[:,t-1] = np.copy(u1)
         return policy,u2
  
-    def opt_policy(self,policy, min_pos):
+    def opt_policy(self,policy, min_pos, verbose):
         """ Find the optimal policy for a given set of minautor positions """
-        time = len(policy[0])
-        actions = [0]*time
+        T = len(policy[0])+1
+        actions = [0]*(T-1)
         self.enemy.pos = min_pos[0]
         self.player.pos = [0,0]
-        actions[0] = policy[self.tostate(self.player.pos, self.player.pos),0]
-        self.display_board()
-        for t in range(0,time-1):
-            self.enemy.pos = min_pos[t+1]
-            self.player.pos = self.player.transition(self.player.pos,self.player.actions[int(actions[t])])
-            actions[t+1] = policy[self.tostate(self.player.pos, self.enemy.pos),t+1]
+        actions[0] = policy[self.tostate(self.player.pos, self.enemy.pos),0]
+        if verbose:
+            print("\n \n Time = 1")
             self.display_board()
-        return actions
-    
-    def proba(self, T):
-        pass
+        for t in range(1,T):
+            self.enemy.pos = min_pos[t]
+            self.player.pos = self.player.transition(self.player.pos,self.player.actions[int(actions[t-1])])
+            if self.enemy.pos == self.player.pos:
+                if verbose:
+                    print("\n \n Time = {}".format(t+1))
+                    self.display_board()
+                    print("\n \n Time = {}".format(t+2))
+                    print(' _____           _                  ')
+                    print('| ____|   __ _  | |_    ___   _ __  ')
+                    print('|  _|    / _  | | __|  / _ \ |  _ \ ')
+                    print('| |___  | (_| | | |_  |  __/ | | | |')
+                    print('|_____|  \__,_|  \__|  \___| |_| |_|')
+                    time.sleep(0.7)
+                return False
+            elif self.player.pos == self.exit_pos and self.enemy.pos != self.player.pos:
+                if verbose:
+                    print("\n \n Time = {}".format(t+1))
+                    self.display_board()
+                    print("\n \n Time = {}".format(t+2))
+                    print(' _____                                           _ ')
+                    print('| ____|  ___    ___    __ _   _ __     ___    __| |')
+                    print('|  _|   / __|  / __|  / _  | |  _ \   / _ \  / _  |')
+                    print('| |___  \__ \ | (__  | (_| | | |_) | |  __/ | (_| |')
+                    print('|_____| |___/  \___|  \__,_| | .__/   \___|  \__,_|')
+                    print('                             |_|                   ')
+                    time.sleep(0.7)
+                return True
+            if t<len(actions):
+                actions[t] = policy[self.tostate(self.player.pos, self.enemy.pos),t]
+            if verbose:
+                    print("\n \n Time = {}".format(t+1))
+                    self.display_board()
+        if verbose:
+                    print("\n \n Time = {}".format(t+2))
+                    print('  _____           _   _              _ ')
+                    print(' |  ___|   __ _  (_) | |   ___    __| |')
+                    print(' | |_     / _` | | | | |  / _ \  / _` |')
+                    print(' |  _|   | (_| | | | | | |  __/ | (_| |')
+                    print(' |_|      \__,_| |_| |_|  \___|  \__,_|')
+                    time.sleep(0.7)
+        return False
+        
+    def proba(self, T, verbose):
+        policies = self.BW_induction(T)[0]
+        iter = 10000
+        win=0
+        for i in range(iter):
+            minotaur_pos = Minotaur.random_path(T)
+            if TheGame.opt_policy(policies, minotaur_pos, verbose):
+                win = win +1
+        return win/iter
 
 
 
@@ -186,7 +233,7 @@ class Player():
 
 class Enemy():
     """docstring  for ClassName"""
-    def __init__(self, can_same=False):
+    def __init__(self, can_same):
         self.pos = [4,4]
         self.can_same = can_same # Can it remain in same place, see transition
 
@@ -310,12 +357,40 @@ class Enemy():
             print('p{{({},{})|({},{})}} = {}'.format(yn,xn,y,x,prob))
 
 Gary = Player()
-Minotaur = Enemy()
+Minotaur = Enemy(True)
 TheGame = Game(Gary, Minotaur)
 
+p,u2 = TheGame.BW_induction(11)
 
-#opt_policies, u2 = TheGame.BW_induction(30)
+#min_pos = [[4,4],[3,4],[3,3],[3,4], [3,3],[3,4], [3,3],[3,4], [3,3],[3,4], [3,3],[3,4], [3,3],[3,4], [3,3]]
+#TheGame.opt_policy(p, min_pos, True)
 
-minotaur_pos = Minotaur.random_path(30)
 
-actions = TheGame.opt_policy(opt_policies, minotaur_pos)
+# Plot the graphs
+
+proba=[]
+Tmin = 2
+Tmax = 40
+for t in range(Tmin,Tmax+1):
+    print(t)
+    proba.append(TheGame.proba(t, False))
+
+if Minotaur.can_same:
+    plt.plot(np.arange(Tmin,Tmax+1),proba)
+    plt.xticks(np.arange(1,Tmax+2,4))
+    plt.xlabel(r'Time available $T$')
+    plt.ylabel(r'Probability to escape the maze $P_T ( \{ Escaped \} )$')
+    plt.savefig('pro_can.ps')
+else:
+    L = [1.0]*30
+    L[0:9] = [0.0]*9
+    L[9:13] = proba 
+    plt.plot(np.arange(2,32),L)
+    plt.plot([12],[L[11]],'ro')
+    plt.text(12,L[11], r'  $(T = 11, P_T=${})'.format(L[11]))
+    plt.xticks(np.arange(1,31,2))
+    plt.xlabel(r'Time available $T$')
+    plt.ylabel(r'Probability to escape the maze $P_T ( \{ Escaped \} )$')
+    plt.savefig('pro_cannot.ps')
+
+
